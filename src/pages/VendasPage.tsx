80d2c9e7f4b1b3f2ef301c300dfc0f79b4f408ba
v0agent <it+v0agent@vercel.com>
2026-06-05 13:12:25 +0000

    feat: refactor VendasPage with new component structure

--- src/pages/VendasPage.tsx
+++ src/pages/VendasPage.tsx
@@ -1,41 +1,37 @@
-import { Hero } from '@/components/Hero'
-import { Blocodeidentificacao } from '@/components/Blocodeidentificacao'
-import { BlocoQuebradecrenca } from '@/components/BlocoQuebradecrenca'
-import { BlocoOproblemareal } from '@/components/BlocoOproblemareal'
-import { BlocoApresentacaodasolucao } from '@/components/BlocoApresentacaodasolucao'
-import { RodadoEcommerce } from '@/components/RodadoEcommerce'
-import { Aprendizado } from '@/components/Aprendizado'
-import { Bonus } from '@/components/Bonus'
-import { BlocoParaQuemENaoE } from '@/components/BlocoParaQuemENaoE'
-import { BrunoBastelli } from '@/components/BrunoBastelli'
-import { Oferta } from '@/components/Oferta'
-import { FAQ } from '@/components/FAQ'
-import { CTAFinal } from '@/components/CTAFinal'
-import { Footer } from '@/components/Footer' 
+import {
+  Header,
+  HeroDirect,
+  KeyPhrasesSection,
+  ProblemSection,
+  SolutionSection,
+  CourseContentSection,
+  EcommerceWheelSection,
+  BonusSection,
+  AudienceSection,
+  OfferSection,
+  FAQSection,
+  FinalCTASection,
+  Footer,
+} from '@/components/landing'
 
 export function VendasPage() {
   return (
     <>
-  <div className="min-h-screen bg-grid">
-    <main>
-      <Hero />
-      <BlocoOproblemareal />
-      <Blocodeidentificacao />
-      <BlocoQuebradecrenca />
-      
-      <BlocoApresentacaodasolucao />
-      <RodadoEcommerce />
-      <Aprendizado />
-      <Bonus />
-      <BlocoParaQuemENaoE />
-      <BrunoBastelli />
-      <Oferta />
-      <FAQ />
-      <CTAFinal />
-    </main>
-
-    <Footer />
-  </div>
-</>
+      <Header />
+      <main>
+        <HeroDirect />
+        <KeyPhrasesSection />
+        <ProblemSection />
+        <SolutionSection />
+        <CourseContentSection />
+        <EcommerceWheelSection />
+        <BonusSection />
+        <AudienceSection />
+        <OfferSection />
+        <FAQSection />
+        <FinalCTASection />
+      </main>
+      <Footer />
+    </>
   )
 }
