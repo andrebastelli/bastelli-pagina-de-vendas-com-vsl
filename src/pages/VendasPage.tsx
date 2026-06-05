518ca9ae5cd7dcafe643057bcb3a70d00681fe8c
v0agent <it+v0agent@vercel.com>
2026-06-05 13:14:14 +0000

    Corrigir importações em VendasPage.tsx
    - Importar componentes corretos do diretório @/components
    - Usar componente ViradadeChave em vez de Blocodeidentificacao
    - Remover importações de landing (estrutura incorreta)
    - Build passa com sucesso

--- src/pages/VendasPage.tsx
+++ src/pages/VendasPage.tsx
@@ -1,35 +1,37 @@
-import {
-  Header,
-  HeroDirect,
-  KeyPhrasesSection,
-  ProblemSection,
-  SolutionSection,
-  CourseContentSection,
-  EcommerceWheelSection,
-  BonusSection,
-  AudienceSection,
-  OfferSection,
-  FAQSection,
-  FinalCTASection,
-  Footer,
-} from '@/components/landing'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { BlocoOproblemareal } from '@/components/BlocoOproblemareal'
+import { ViradadeChave } from '@/components/ViradadeChave'
+import { BlocoQuebradecrenca } from '@/components/BlocoQuebradecrenca'
+import { BlocoApresentacaodasolucao } from '@/components/BlocoApresentacaodasolucao'
+import { RodadoEcommerce } from '@/components/RodadoEcommerce'
+import { Aprendizado } from '@/components/Aprendizado'
+import { Bonus } from '@/components/Bonus'
+import { BlocoParaQuemENaoE } from '@/components/BlocoParaQuemENaoE'
+import { BrunoBastelli } from '@/components/BrunoBastelli'
+import { Oferta } from '@/components/Oferta'
+import { FAQ } from '@/components/FAQ'
+import { CTAFinal } from '@/components/CTAFinal'
+import { Footer } from '@/components/Footer'
 
 export function VendasPage() {
   return (
     <>
       <Header />
       <main>
-        <HeroDirect />
-        <KeyPhrasesSection />
-        <ProblemSection />
-        <SolutionSection />
-        <CourseContentSection />
-        <EcommerceWheelSection />
-        <BonusSection />
-        <AudienceSection />
-        <OfferSection />
-        <FAQSection />
-        <FinalCTASection />
+        <Hero />
+        <BlocoOproblemareal />
+        <ViradadeChave />
+        <BlocoQuebradecrenca />
+        <BlocoApresentacaodasolucao />
+        <RodadoEcommerce />
+        <Aprendizado />
+        <Bonus />
+        <BlocoParaQuemENaoE />
+        <BrunoBastelli />
+        <Oferta />
+        <FAQ />
+        <CTAFinal />
       </main>
       <Footer />
     </>
